977dd8212dd2efc3fbff9e21ea84dc7ca1e745c7
Hoangminhfcb <78634684+Hoangminhfcb@users.noreply.github.com>
2025-11-05 10:07:13 +0700

    ok

--- app/src/main/java/com/example/garapro/services/NotificationService.kt
+++ app/src/main/java/com/example/garapro/services/NotificationService.kt
@@ -18,6 +18,7 @@ class NotificationService: FirebaseMessagingService() {
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // FCM registration token to your app server.
+
         //sendRegistrationToServer(token)
     }
 
