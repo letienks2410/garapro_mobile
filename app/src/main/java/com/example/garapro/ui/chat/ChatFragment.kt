a30013f170dd62e765cec3d59087c7bf765de1a1
Hoangminhfcb <78634684+Hoangminhfcb@users.noreply.github.com>
2025-12-13 17:12:37 +0700

    fix

--- app/src/main/java/com/example/garapro/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/garapro/ui/chat/ChatFragment.kt
@@ -43,7 +43,7 @@ class ChatFragment : Fragment() {
     private lateinit var layoutWaiting: LinearLayout
 
     // Thay bằng webhook của bạn
-    private val WEBHOOK_URL = "https://n8n.zanis.id.vn/webhook/b396fc96-535d-4af6-8f1a-33ea454b8bb2"
+    private val WEBHOOK_URL = "https://n8n.zanis.id.vn/webhook-test/4c025dbd-e79c-4d24-93e1-c9238c388964"
     private val AUTH_TOKEN: String? = null // nếu cần: "Bearer xxxxx"
 
     private val client = OkHttpClient()
