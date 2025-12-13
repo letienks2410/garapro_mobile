7fd124a35feb92a9068c434e44e9dce5320c449d
letienks2410 <anhemgameplay@gmail.com>
2025-12-14 01:50:54 +0700

    a

--- app/src/main/java/com/example/garapro/utils/Constants.kt
+++ app/src/main/java/com/example/garapro/utils/Constants.kt
@@ -2,11 +2,11 @@ package com.example.garapro.utils
 
 object Constants {
 
-//    const val BASE_URL_SIGNALR = "http://103.216.119.34:5000"
-    const val BASE_URL_SIGNALR = "http://192.168.1.129:5117"
-//    const val BASE_URL = "http://103.216.119.34:5000/api/"
-    const val BASE_URL = "http://192.168.1.129:5117/api/"
-
+    const val BASE_URL_SIGNALR = "http://103.216.119.34:5000"
+//    const val BASE_URL_SIGNALR = "http://192.168.1.129:5117"
+    const val BASE_URL = "http://103.216.119.34:5000/api/"
+//    const val BASE_URL = "http://192.168.1.129:5117/api/"
+a
 
 
     //  const val BASE_URL = "http://192.168.1.183:5117/api/"
